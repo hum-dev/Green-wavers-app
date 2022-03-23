9e470f4f4d97d3bc879a770f134565c69d5e1028
Humkym <humkymh@outlook.com>
2022-03-23 11:27:54 +0300

    added a home section

---
+++ HOMEPAGE/app.js
@@ -0,0 +1,54 @@
+let slides = document.querySelectorAll('.carousel');
+let btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+
+let manualNav = function(manual) {
+    slides.forEach((carousel) => {
+        carousel.classList.remove('active');
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+
+    });
+
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    });
+});
+
+let repeat = function(activeClass) {
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    let repeater = () => {
+        setTimeout(function() {
+
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            });
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+
+            if(slides.length == i) {
+                i = 0;
+            }
+            if(i >= slides.length) {
+                return;
+            }
+            repeater();
+        }, 1000);
+    }
+    repeater ();
+}
+
+repeat();
